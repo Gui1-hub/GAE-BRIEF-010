6a56c609bb839616140cac806d4969b1a617e741
Guilhem <guilhem.alberge@gmail.com>
2024-12-12 14:30:25 +0100

    toto

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
+# Provider Azure
 provider "azurerm" {
   features {}
   subscription_id = "09eef35c-bc62-40ab-8932-c171977b897b"
 }
-# Définir le groupe de ressources
+
+# Groupe de ressources
 resource "azurerm_resource_group" "resource_group" {
   name     = "GAE-TEST-RG"
   location = "FranceCentral"
 }
 
-# Définir le réseau virtuel
+# Réseau virtuel
 resource "azurerm_virtual_network" "virtual_network" {
   name                = "GAE-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -16,7 +18,7 @@ resource "azurerm_virtual_network" "virtual_network" {
   resource_group_name = azurerm_resource_group.resource_group.name
 }
 
-# Définir le sous-réseau
+# Sous-réseau
 resource "azurerm_subnet" "subnet" {
   name                 = "GAE-TEST-subnet"
   resource_group_name  = azurerm_resource_group.resource_group.name
@@ -24,15 +26,40 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Définir l'adresse IP publique
+# NSG (Groupe de sécurité réseau)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "GAE-TEST-NSG"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Association explicite NSG <-> Sous-réseau
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Adresse IP publique
 resource "azurerm_public_ip" "public_ip" {
   name                = "GAE-TEST-public-ip"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
-# Définir l'interface réseau
+# Interface réseau
 resource "azurerm_network_interface" "network_interface" {
   name                = "GAE-TEST-NIC"
   location            = azurerm_resource_group.resource_group.location
@@ -46,17 +73,23 @@ resource "azurerm_network_interface" "network_interface" {
   }
 }
 
-# Créer la machine virtuelle Ubuntu
+# Machine virtuelle Ubuntu
 resource "azurerm_linux_virtual_machine" "virtual_machine" {
   name                = "GAE-TEST-ubuntu-vm"
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
-  size                = "Standard_B2ms"
+  size                = "Standard_B4ls_v2"
   admin_username      = "adminsimplon"
   admin_password      = "P@ssw0rd123!"
   network_interface_ids = [
     azurerm_network_interface.network_interface.id,
   ]
+
+  # Clé SSH pour l'authentification
+  admin_ssh_key {
+    username   = "adminsimplon"
+    public_key = file("C:/VSC/Brief-10/ssh/mykey.pub") # Chargez votre clé publique ici
+  }
 
   os_disk {
     caching              = "ReadWrite"
@@ -65,8 +98,8 @@ resource "azurerm_linux_virtual_machine" "virtual_machine" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22.04-LTS"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
     version   = "latest"
   }
 }
